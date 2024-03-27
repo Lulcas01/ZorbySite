d305d56bef0866cb8434eddc7cac7860692a043a
Lucas Alves <l.abeats2238@gmail.com>
2024-03-27 12:18:20 -0300

    favicon mudado e titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Section from './components/Section';
 import Footer from './components/Footer';
 import Metas from './components/Metas'
 import './App.css';
